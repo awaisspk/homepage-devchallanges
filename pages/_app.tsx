639d118d3d3a1f7252a7ae1bb26edf367c0d6350
awaisspk <iamawaisiqball@gmail.com>
2021-12-12 20:43:01 +0500

    Feat: Button comoponent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
+import {Layout} from '@components/Layout';
 import {globalStyles} from '@globalCss';
 import type {AppProps} from 'next/app';
 
 function MyApp({Component, pageProps}: AppProps) {
   globalStyles();
-  return <Component {...pageProps} />;
+  return (
+    <Layout header footer>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
